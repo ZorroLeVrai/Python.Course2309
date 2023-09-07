5acaa72734d67261808c8b784bb38d045361eccc
ZorroLeVrai <69362591+ZorroLeVrai@users.noreply.github.com>
2023-09-07 14:51:11 +0200

    Update correction

--- Fibonacci/main.py
+++ Fibonacci/main.py
@@ -1,18 +1,4 @@
-def fibo1(nb):
-    if nb < 2:
-        return nb
-        
-    nm2 = 0
-    nm1 = 1
-    for i in range(2, nb+1):
-        result = nm2 + nm1
-        nm2 = nm1
-        nm1 = result
-    
-    return nm1
-
-
-# def fibo2(nb):
+def fibo(nb):
     if nb < 2:
         return nb
         
@@ -23,5 +9,5 @@ def fibo1(nb):
     
     return nm1
 
-print(fibo1(4))
-#print(fibo2(15))
+print(fibo(4))
+#print(fibo(15))
---
+++ FizzBuzz/fizzbuzz.py
@@ -0,0 +1,29 @@
+file_name = "fizzBuzz.txt"
+
+def fizz_buzz(n):
+    result = ""
+    if n % 3 == 0:
+        result += "Fizz"
+    if n % 5 == 0:
+        result += "Buzz"
+    return result if result else str(n)
+
+def create_fb_list(debut, fin):
+    return list(map(lambda n:fizz_buzz(n),range(debut, fin+1)))
+
+def print_in_file(text, file_name):
+    with open(file_name, "wt") as file:
+        file.write(text)
+
+def input_int(message):
+    while True:
+        try:
+            return int(input(f"{message} "))
+        except:
+            print("Vous devez saisir un entier\n")
+
+
+nb_depart = input_int("Saisissez le nombre de départ:")
+nb_fin = input_int("Saisissez le nombre de fin:")
+fizzBuzz_list = create_fb_list(nb_depart, nb_fin)
+print_in_file("\n".join(fizzBuzz_list), file_name)
