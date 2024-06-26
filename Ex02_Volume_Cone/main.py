9463117e94020dee8af815f811cfce49a811a7b5
ZorroLeVrai <69362591+ZorroLeVrai@users.noreply.github.com>
2024-06-26 14:22:01 +0200

    Improve exercise 2 solution

--- Ex02_Volume_Cone/main.py
+++ Ex02_Volume_Cone/main.py
@@ -1,6 +1,12 @@
 import math
 
-rayon = float(input("Entrez le rayon: "))
-hauteur = float(input("Entrez la hauteur: "))
-volume = hauteur*(math.pi*rayon**2) / 3
-print(f"Le volume est de {volume}")
+# Les entrées de l'utilisateur / utilisatrice
+rayon_cm = float(input("Entrez le rayon (en cm): "))
+hauteur_cm = float(input("Entrez la hauteur (en cm): "))
+
+#Les calculs
+base_cm2 = math.pi*rayon_cm**2
+volume_cm3 = hauteur_cm*base_cm2 / 3
+
+#Affchage du résultat
+print(f"Le volume est de {round(volume_cm3, 2)} cm\u00b3")
